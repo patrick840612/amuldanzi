4b8ddc628a853e60d4f07a4a9047a0cf827d93ed
HoJun_ <acepilot0908@gmail.com>
2023-06-30 16:10:08 +0900

    최초설정

---
+++ amuldanzi/src/main/java/com/amuldanzi/AmuldanziApplication.java
@@ -0,0 +1,13 @@
+package com.amuldanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmuldanziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmuldanziApplication.class, args);
+	}
+
+}
---
+++ amuldanzi/src/test/java/com/amuldanzi/AmuldanziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amuldanzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmuldanziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
