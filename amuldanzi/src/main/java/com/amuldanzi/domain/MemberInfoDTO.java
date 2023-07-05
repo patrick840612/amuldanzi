ee17fe38d901b954e228f613249c8344654ca688
patrick1004-admin <leekoon11@gmail.com>
2023-07-05 17:55:18 +0900

    회원 정보 DTO 변수선언만

--- amuldanzi/src/main/java/com/amuldanzi/domain/MemberInfoDTO.java
+++ amuldanzi/src/main/java/com/amuldanzi/domain/MemberInfoDTO.java
@@ -5,4 +5,13 @@ import lombok.Data;
 @Data
 public class MemberInfoDTO {
 
+	private String 	id;
+	private String 	userPassword;
+	private String 	userEmail;
+	private String 	userName;
+	private String 	userTel;
+	private String 	userAddr;
+	private String 	character;
+	private boolean marketingOk;
+	private String 	memberRole;
 }
