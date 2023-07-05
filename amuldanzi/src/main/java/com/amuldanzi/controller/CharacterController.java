df34339fff2acf162b4619ba5b3dd275b2091a4f
patrick1004-admin <leekoon11@gmail.com>
2023-07-05 17:16:59 +0900

    이병훈 폴더 및 파일 생성

---
+++ amuldanzi/src/main/java/com/amuldanzi/controller/CharacterController.java
@@ -0,0 +1,15 @@
+package com.amuldanzi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/character")
+public class CharacterController {
+	
+	@RequestMapping("/character")
+	public void character() {
+		
+	}
+	
+}
