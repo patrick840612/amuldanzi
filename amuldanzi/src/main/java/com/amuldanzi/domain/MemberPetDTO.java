23d446fdd223a95d9f857ce73d9b21fe4986bffb
patrick1004-admin <leekoon11@gmail.com>
2023-07-15 18:30:38 +0900

    테이블 충돌 해결

---
+++ amuldanzi/src/main/java/com/amuldanzi/domain/MemberPetDTO.java
@@ -0,0 +1,47 @@
+package com.amuldanzi.domain;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "memberPet")
+@DynamicInsert
+public class MemberPetDTO {
+
+
+
+	@Id // pk 지정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(nullable = false, updatable = false, insertable = false)
+	private Integer memberPetId;
+	
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name="id", referencedColumnName = "id")
+	private MemberInfoDTO memberId;
+	
+	private String petName;
+	private String whichPet;
+	private String petBlood;
+
+	private String gps;
+	
+	public MemberPetDTO(String name, String breed) {
+
+	    this.petName = petName;
+	    this.whichPet = whichPet;
+	    this.petBlood = petBlood;
+	    this.gps = gps;
+	}
+
+}
