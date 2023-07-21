212d54d6d76103dbe05411c6deddf7742d500c79
patrick1004-admin <leekoon11@gmail.com>
2023-07-21 12:58:04 +0900

    케어 컨트롤러 완료

--- amuldanzi/src/main/java/com/amuldanzi/controller/CareController.java
+++ amuldanzi/src/main/java/com/amuldanzi/controller/CareController.java
@@ -1,6 +1,8 @@
 package com.amuldanzi.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.amuldanzi.domain.CareDTO;
 import com.amuldanzi.service.CareService;
+import com.amuldanzi.service.LoginService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/care")
@@ -18,6 +24,12 @@ public class CareController {
 	@Autowired
 	CareService service;
 	
+	@Autowired
+	private LoginService loginService;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
 	@RequestMapping("/care")
 	public void care(Model m) {
 		
@@ -25,7 +37,40 @@ public class CareController {
 		m.addAttribute("careList", careList);
 		System.out.println(careList);
 		
+		Map<String,Object> map = headerChange();
+	    m.addAttribute("memberRole", map.get("memberRole"));
+		
 	}
+	
+	// 페이지 이동시 회원역할에 따른 헤더 변경하기 정보 얻기 함수
+	private Map<String,Object> headerChange() {
+		Map<String,Object> map = new HashMap<String, Object>();
+        Cookie[] cookies = request.getCookies();
+        String accessToken = null;
+        
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("access_token".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+		if(accessToken != null) {
+	        String id = (String)loginService.getClaims(accessToken).get("id");
+	        String memberRole = (String)loginService.getClaims(accessToken).get("memberRole");
+	        map.put("memberRole", memberRole);
+	        map.put("id", id);
+
+		}else {
+	        map.put("memberRole", "");
+	        map.put("id", "");
+
+		}
+		return map;
+		
+	}// 페이지 이동시 회원역할에 따른 헤더 변경하기 끝
 	
 	@RequestMapping("/careDetail")
 	public void careDetail(@RequestParam("id") Integer careId, Model m) {
