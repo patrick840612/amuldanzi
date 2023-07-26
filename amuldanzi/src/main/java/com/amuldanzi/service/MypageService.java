d002f2d60c6d3df335b124d9b04c5a9fe644925c
patrick1004-admin <leekoon11@gmail.com>
2023-07-26 20:32:21 +0900

    회원 정보 관리 완료

--- amuldanzi/src/main/java/com/amuldanzi/service/MypageService.java
+++ amuldanzi/src/main/java/com/amuldanzi/service/MypageService.java
@@ -10,5 +10,8 @@ public interface MypageService {
 	public List<MemberPetDTO> selectById(MemberInfoDTO member);
 	public List<MemberSocialDTO> selectSocialById(MemberInfoDTO member);
 	public void deleteSocial(MemberSocialDTO memberSocial);
-
+	public void updateInfo(MemberInfoDTO member, List<MemberPetDTO> petList);
+	public void deletePet(MemberPetDTO pet);
+	public void petDel(MemberInfoDTO member);
+	
 }
--- amuldanzi/src/main/java/com/amuldanzi/service/MypageServiceImpl.java
+++ amuldanzi/src/main/java/com/amuldanzi/service/MypageServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.amuldanzi.dao.LoginDAO;
 import com.amuldanzi.dao.LoginPetDAO;
 import com.amuldanzi.dao.LoginSocialDAO;
 import com.amuldanzi.domain.MemberInfoDTO;
@@ -20,6 +22,9 @@ public class MypageServiceImpl implements MypageService {
 	
 	@Autowired
 	LoginSocialDAO loginSocialDAO;
+	
+	@Autowired
+	LoginDAO loginDAO;
 	
 	// pet 정보 가져오기(회원정보관리 입장시)
 	@Override
@@ -50,4 +55,39 @@ public class MypageServiceImpl implements MypageService {
 	public void deleteSocial(MemberSocialDTO memberSocial) {
 		loginSocialDAO.deleteByMemberId(memberSocial);
 	}
+	
+	// 회원정보 업데이트
+	@Override
+	@Transactional
+	public void updateInfo(MemberInfoDTO member, List<MemberPetDTO> petList) {
+		loginDAO.updateById(member);
+		List<MemberPetDTO> dbPets = loginPetDAO.selectbyId(member);
+
+		if (petList != null) {
+			for (MemberPetDTO pet : petList) {
+				if (pet.getMemberPetId() == 0) {
+					// 1. petList에 있는 DTO의 memberPetId의 값이 0이면 save
+					loginPetDAO.save(pet);
+				} else {
+					// 2. petList에 있는 DTO의 memberPetId의 값이 db에 있으면 수정
+					loginPetDAO.updatePet(pet);
+
+				}
+			}
+		}
+	}
+
+	// 반려동물 삭제
+	@Override	
+	public void deletePet(MemberPetDTO pet) {
+		loginPetDAO.deletePet(pet.getMemberPetId());
+	}
+	
+	// 반려동물 전체 삭제
+	@Override
+	public void petDel(MemberInfoDTO member) {
+		loginPetDAO.petDel(member);
+		loginDAO.changBloodGive(member);
+	}
+	
 }
