f8d0fbea75f119fa9dbe494c6f7b91d6dd29b28c
DESKTOP-FGUETDQ\kosmo <ghkddbswo127@naver.com>
2023-07-29 18:32:00 +0900

    엘라스틱서치 공지글 커밋이요

--- amuldanzi/src/main/java/com/amuldanzi/persistence/MapRepository.java
+++ amuldanzi/src/main/java/com/amuldanzi/persistence/MapRepository.java
@@ -13,7 +13,7 @@ public interface MapRepository extends CrudRepository<MemberPetDTO, String>{
 	// MapDTO에 저장되어있고 addr 을 포함한 검색결과 추출
 	//@Query("SELECT new HashMap(mi.sido as sido, mi.userTel as userTel, m.whichPet as whichPet, m.petBlood as petBlood) FROM MapDTO m JOIN m.memberId mi WHERE mi.sido LIKE CONCAT('%', :sido, '%')")
 	@Query(value = "SELECT mi.sido sido, mi.blood_tel blood_tel, mp.which_pet whichPet, mp.pet_blood petBlood \r\n"
-			+ "FROM member_info mi \r\n"
-			+ "JOIN member_pet mp ON mi.id = mp.id WHERE mi.sido LIKE CONCAT('%', :sido, '%')", nativeQuery = true)
+	        + "FROM member_info mi \r\n"
+	        + "JOIN member_pet mp ON mi.id = mp.id WHERE LEFT(mi.sido, 2) = LEFT(:sido, 2)", nativeQuery = true)
 	List<Object[]> findByLocation(@Param("sido") String sido);
 }
